5e12bb71d7ddd287d38594ab5d2be543689bd63f
yangchoutesting <66073287+yangchoutesting@users.noreply.github.com>
2021-12-19 11:38:51 +0800

    refactor the code

--- binarySearch.js
+++ binarySearch.js
@@ -1,32 +1,65 @@
-function binarySearch_r(array, data, left, right) {
-  if (left < right) {
-    let mid = Math.floor((left + right) / 2);
-    if (data === array[mid]) {
-      console.log("found! " + data);
-      return mid;
-    } else if (data < array[mid])
-      return binarySearch_r(array, data, left, mid - 1);
-    else return binarySearch_r(array, data, mid + 1, right);
+function binarySearch_r(array, target, left, right) {
+  if (array[left] === target) {
+    return left;
   }
+  if (array[right] === target) {
+    return right;
+  }
+
+  if (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (array[mid] === target) {
+      return mid;
+    }
+
+    if (array[mid] > target) {
+      return binarySearch_r(array, target, left, mid - 1);
+    }
+
+    if (array[mid] <= target) {
+      return binarySearch_r(array, target, mid + 1, right);
+    }
+  }
+
   return -1;
 }
 
-function binarySearch_i(array, size, data) {
+function binarySearch_i(array, target) {
   let left = 0;
-  let right = size - 1;
-  let mid = 0;
+  let right = array.length - 1;
 
-  while (left < right) {
-    mid = Math.floor((left + right) / 2);
-    if (array[mid] === data) {
-      console.log("found! " + data);
-      return mid;
-    } else if (data[mid] < data) left = mid + 1;
-    else right = mid - 1;
+  if (array[left] === target) {
+    return left;
   }
+  if (array[right] === target) {
+    return right;
+  }
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (array[mid] === target) {
+      return mid;
+    }
+
+    if (array[mid] < target) {
+      left = mid + 1;
+      continue;
+    }
+
+    if (array[mid] >= target) {
+      right = mid - 1;
+      continue;
+    }
+  }
+
   return -1;
 }
 
 array = [1, 2, 6, 8, 9, 13, 26, 67, 78];
-console.log(binarySearch_r(array, 9, 0, 9));
-console.log(binarySearch_i(array, array.length, 6));
+for (let i = 1; i < 10; i++) {
+  console.log(`find ${i}`);
+  console.log(`Found it at : ${binarySearch_r(array, i, 0, array.length)}`);
+  console.log(`Fount it at : ${binarySearch_i(array, i)}`);
+}
