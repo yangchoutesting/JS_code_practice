68dfc50a6bfa18364ea4d5d863aa37fa31fa0295
yangchoutesting <66073287+yangchoutesting@users.noreply.github.com>
2021-12-19 11:50:16 +0800

    refactor the code

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,10 +1,8 @@
-function bubbleSort(array, N) {
-  let temp = 0;
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 1; j < N - i; j++) {
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length - i; j++) {
       if (array[j] < array[j - 1]) {
-        temp = array[j - 1];
+        let temp = array[j - 1];
         array[j - 1] = array[j];
         array[j] = temp;
       }
@@ -13,6 +11,6 @@ function bubbleSort(array, N) {
   return array;
 }
 
-data = [1, 6, 4, 3, 5, 2];
-console.log(data);
-console.log(bubbleSort(data, data.length));
+array = [1, 6, 4, 3, 5, 2];
+console.log(`unsorted: ${array}`);
+console.log(`sorted: ${bubbleSort(array)}`);
