e5f317694cdde11222a40e707134eb96029bb7a3
揚 <66073287+yangchoutesting@users.noreply.github.com>
2022-05-15 22:30:50 +0800

    find common maximum in different arrays
    - interview question
    - ES6 feature practice

---
+++ findCommonMax.js
@@ -0,0 +1,50 @@
+function testCase() {
+	let arr1 = [1, 2, 4, 5, 6];
+	let arr2 = [3, 4, 5, 6];
+	let arr3 = [4, 5, 6];
+	return [arr1, arr2, arr3];
+}
+
+
+function main() {
+	const [arr1, arr2, arr3] = testCase();
+	const result = findCommonMax(arr1, arr2, arr3);
+	
+	console.log(`The common maximum number of arrays is ${result}.`);
+}
+
+
+function findCommonMax(arr1, arr2, arr3) {
+	let map = new Map();
+	addNumberCountToMap(arr1, map);
+	addNumberCountToMap(arr2, map); 
+	addNumberCountToMap(arr3, map); 
+	
+	return getCommonMax(map);
+}
+
+
+function addNumberCountToMap(arr, map) {
+	for (let num of arr) {
+		if (map.has(num)) {
+			map.set(num, map.get(num) + 1);
+			continue;
+		}
+		map.set(num, 1);
+	}
+	return map;
+}
+
+
+function getCommonMax(map) {
+	let tempMax = -1;
+	for (let [key, value] of map) {
+		if (value >= 3 && key > tempMax) {
+			tempMax = key;
+		}
+	}
+	return tempMax;
+}
+
+
+main();
